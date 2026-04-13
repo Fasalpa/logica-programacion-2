5a5d6686a2e37024697e211f918ede4e66abeaa7
fasalpa <psfr97@gmail.com>
2026-04-12 23:52:42 -0500

    Limpiar Código

--- app.js
+++ app.js
@@ -9,19 +9,16 @@ btnConvertir.addEventListener("click", function () {
 
   if (inpCelsius.value.trim() === "") {
     alert("El campo Celsius no tiene ningún valor");
-    inpCelsius.value = "";
+  } else if (isNaN(valorCelsius)) {
+    alert("El dato ingresado no es un número. Ingrese un número.");
   } else {
-    if (isNaN(valorCelsius)) {
-      alert("El dato ingresado no es un número. Ingrese un número.");
-    } else {
-      fahrenheit = valorCelsius * 1.8 + 32;
-      kelvin = valorCelsius + 273.15;
-      // alert(kelvin) ya comprobé que funciona
-      spanCelsius.textContent = valorCelsius;
-      //   console.log(`este es el valor de ${valorCelsius}`); ahora compruebo que el cambio span funciona
-      spanFahrenheit.textContent = fahrenheit;
-      spanKelvin.textContent = kelvin;
-      inpCelsius.value = "";
-    }
+    let fahrenheit = valorCelsius * 1.8 + 32;
+    let kelvin = valorCelsius + 273.15;
+    // alert(kelvin) ya comprobé que funciona
+    spanCelsius.textContent = valorCelsius;
+    //   console.log(`este es el valor de ${valorCelsius}`); ahora compruebo que el cambio span funciona
+    spanFahrenheit.textContent = fahrenheit;
+    spanKelvin.textContent = kelvin;
+    inpCelsius.value = "";
   }
 });
